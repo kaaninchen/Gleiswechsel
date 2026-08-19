cd2d9685451bf7142337c3cd0c9fe231d89ee287
Kaaninchen <124433727+kaaninchen@users.noreply.github.com>
2026-08-19 11:48:29 +0200

    operators.py: slogans

--- src/data/operators.py
+++ src/data/operators.py
@@ -1,55 +1,67 @@
 OPERATORS = {
     "Abellio Rail Mitteldeutschland GmbH": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Abellio_logo.svg/1920px-Abellio_logo.svg.png",
-        "color": 0xD7002E
+        "color": 0xD7002E,
+        "slogan": ["Freie Bahn für deine Ziele!"]
     },
     "Agilis": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Agilis_Logo.svg/960px-Agilis_Logo.svg.png",
-        "color": 0xCAE15B
+        "color": 0xCAE15B,
+        "slogan": ["Reinsetzen, zurücklehnen", "Fahren Sie mit uns. Nehmen Sie Platz."]
     },
     "Arverio Bayern": {
         "logo": "https://cdn.discordapp.com/attachments/1383843132906537023/1528006572805062776/Arverio_Avi_Bayern_blau_RGB.png?ex=6a5cba83&is=6a5b6903&hm=5781ae6c372c92ab4f52409c6fc91e5014ac34ccf24db665ade052e8135bdde0&animated=true",
-        "color": 0x0083BE
+        "color": 0x0083BE,
+        "slogan": ["Dein Weg ist unser Ziel."]
     },
     "Berliner Verkehrsbetriebe": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/BVG_Logo_07.2021.svg/960px-BVG_Logo_07.2021.svg.png",
-        "color": 0xEFD13C
+        "color": 0xEFD13C,
+        "slogan": ["Weil wir dich lieben"]
     },
     "DSB": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/DSB_company_logo.svg/960px-DSB_company_logo.svg.png",
-        "color": 0xB22B32
+        "color": 0xB22B32,
+        "slogan": ["Tak for at vise hensyn"]
     },
     "enno": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Enno_logo.svg/960px-Enno_logo.svg.png",
-        "color": 0x88216F
+        "color": 0x88216F,
+        "slogan": ["enno fährt bunt"]
     },
     "Erfurter Bahn GmbH": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Erfurter_Bahn_logo.svg/960px-Erfurter_Bahn_logo.svg.png",
-        "color": 0x009133
+        "color": 0x009133,
+        "slogan": ["Kommt gut an."]
     },
     "eurobahn": {
         "logo": "https://cdn.discordapp.com/attachments/1383843132906537023/1528405390260179146/Eurpnajm.png?ex=6a5e2df1&is=6a5cdc71&hm=d73fc3458f59f8942a5a0f51311308b12567b35b55b64aab0726bf6be92b3b0e&animated=true",
-        "color": 0x005a9b
+        "color": 0x005a9b,
+        "slogan": ["Echte Menschen. Echt schöne Ziele."]
     },
     "European Sleeper": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/European_Sleeper_Logo.svg/960px-European_Sleeper_Logo.svg.png",
-        "color": 0xEB4A27
+        "color": 0xEB4A27,
+        "slogan": ["The Good Night Train", "Travel while you sleep"]
     },
     "Flixbus": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flixbus_201x_logo.svg/1280px-Flixbus_201x_logo.svg.png",
-        "color": 0x8CD541
+        "color": 0x8CD541,
+        "slogan": ["Einfach Busfahren.", "Fahr grün!", "FLIX it", "Mehr Leben im echten Leben"]
     },
     "GoVolta": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/GoVolta_logo.svg/1280px-GoVolta_logo.svg.png",
-        "color": 0x0B70F6
+        "color": 0x0B70F6,
+        "slogan": ["Internationale Züge, endlich erschwinglich!", "International trains, finally affordable!"]
     },
     "GVB": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/BSicon_LOGO_GVB.svg/960px-BSicon_LOGO_GVB.svg.png",
-        "color": 0x2B62AF
+        "color": 0x2B62AF,
     },
     "Nordbahn Eisenbahngesellschaft": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/b/b5/Logo_Nordbahn_NAH.SH_Blau_positiv_final.png",
-        "color": 0x1A2848
+        "color": 0x1A2848,
+        "slogan": ["nordbahn kommt von Norden", "Bringt den Norden näher", "Wir transportieren das Biest"]
     },
     "NS": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Logo_NS.svg/960px-Logo_NS.svg.png",
@@ -58,23 +70,27 @@ OPERATORS = {
     },
     "Ostdeutsche Eisenbahn GmbH": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/ODEG-Logo_Neu.svg/960px-ODEG-Logo_Neu.svg.png",
-        "color": 0x00745C
+        "color": 0x00745C,
+        "slogan": ["Rücksicht kommt gut", "Benimm dich, als wäre Mutti dabei.", "Sicher mit der Sachsenschleuder"]
     },
     "Regionalverkehre Start Deutschland GmbH (Start Mitteldeutschland)": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Logo_der_Regionalverkehre_Start_Deutschland.svg/960px-Logo_der_Regionalverkehre_Start_Deutschland.svg.png",
-        "color": 0x61A731
+        "color": 0x61A731,
     },
     "S-Bahn Hannover (Transdev)": {
         "logo": "https://cdn.discordapp.com/attachments/1526274436523888732/1528470315234234520/sbahn.png?ex=6a5e6a68&is=6a5d18e8&hm=c9f630c6f0c646eafeccef9b4d591ce8fcf8856b2b45f14e72fbf25e32edb017&animated=true",
-        "color": 0x1A4389
+        "color": 0x1A4389,
+        "slogan": ["Wir geben alles", "Wir bringen dich", "Wir bringen Respekt"]
     },
     "SBB": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/SBB_logo_simplified.svg/960px-SBB_logo_simplified.svg.png",
-        "color": 0xEB0000
+        "color": 0xEB0000,
+        "slogan": ["Der Kluge reist im Zuge", "SBB schafft Verbindungen", "Unterwegs zuhause"]
     },
     "SNCF": {
         "logo": "https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Sncf-logo.svg/960px-Sncf-logo.svg.png",
-        "color": 0x812B6D
+        "color": 0x812B6D,
+        "slogan": ["Pour nous tous", "Donner au train des idées d'avance"]
     },
     "Tallink Grupp AS": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Tallink_logo.svg/330px-Tallink_logo.svg.png",
@@ -82,15 +98,18 @@ OPERATORS = {
     },
     "Vr": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Vr_Logo.png/330px-Vr_Logo.png",
-        "color": 0x00B451
+        "color": 0x00B451,
+        "slogan": ["Aikaa yhteisellä matkalla"]
     },
     "Westbahn Management GmbH": {
         "logo": "https://corporate.westbahn.at/uploads/Logos/westbahn2025-logo-signet-small.png",
-        "color": 0x1D4A83
+        "color": 0x1D4A83,
+        "slogan": ["Immer ein guter Zug", "Da steig ich ein", "Jetzt aber Westbahn"]
     },
     "WestfalenBahn": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Westfalenbahn_logo.svg/330px-Westfalenbahn_logo.svg.png",
-        "color": 0x48A1DD
+        "color": 0x48A1DD,
+        "slogan": ["Weil's meine Region ist", "Hier bin ich daheim!"]
     },
     "db_allgemein": {
         "logo": "https://marketingportal.extranet.deutschebahn.com/resource/blob/13602522/c53f806b9df966e144010b276af72dd2/Bild_09-data.png",
@@ -104,7 +123,8 @@ OPERATORS = {
     },
     "db_bayern": {
         "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Bahnland_Bayern_Logo_2021.svg/500px-Bahnland_Bayern_Logo_2021.svg.png",
-        "color": 0x0095DB
+        "color": 0x0095DB,
+        "slogan": ["Zeit für dich", "Bringt dich vorwärts", "Wir bewegen das Bahnland Bayern", "Ey, i muas da ehrlich sogen i wünimma mit dir zaum sein, weil i a eif agrod gmerkt hob das i nix ernstes grod will und es liegt wirkli ned an dir oba es passt halt ned sound du müsstest holt a nü des abhoin wos du bei mir vergessen host"]
     },
     "fallback": {
         "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCVeC5E0mNNBKyQftQaFMzxIVkbDvEnSzWv07h_c8PdA&s=10",
